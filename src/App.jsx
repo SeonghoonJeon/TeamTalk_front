2a395feac006563c7445a6897cb1723f5e04e6e1
meroKang <robbin6301@gmail.com>
2025-11-11 00:54:42 +0900

    login page

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/Signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
